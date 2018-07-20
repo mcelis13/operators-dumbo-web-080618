a191e2fa4f84e3adb7ae0b94dccabc02d9f0a1b1
Madeline Celis <madelinebcelis@gmail.com>
2018-07-20 17:17:44 +0000

    automatically backed up by learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -11,8 +11,8 @@ end
 
 
 def not_safe?(speed)
-	if speed > 40 && speed < 60 ? return false : true
-  elsif speed > 60 ? return true : false
-	elsif speed < 40 ? return true : false
+	if return speed > 40 && speed < 60 ? false : true
+  elsif return speed > 60 ? true : false
+	elsif speed < 40 ? true : false
 	end
 end
